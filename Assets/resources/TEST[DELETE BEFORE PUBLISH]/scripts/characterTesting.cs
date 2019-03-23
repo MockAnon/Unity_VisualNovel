8998c3dc453c3d569c140d43e862545c154b8b59
Jaron Evans <jarondevans@gmail.com>
2019-03-22 22:46:30 -0400

    git delete before final update

--- Assets/resources/TEST[DELETE BEFORE PUBLISH]/scripts/characterTesting.cs
+++ Assets/resources/TEST[DELETE BEFORE PUBLISH]/scripts/characterTesting.cs
@@ -5,25 +5,33 @@ using UnityEngine;
 public class characterTesting : MonoBehaviour 
 {
     public Character Raelin;
+    public Character Avira;
 
 	// Use this for initialization
 	void Start () 
     {
         Raelin = CharacterManager.instance.GetCharacter ("Raelin", enableCreatedCharacterOnStart: false);	
+        Avira = CharacterManager.instance.GetCharacter("Avira", enableCreatedCharacterOnStart: false);
 	}
 
     public string[] speech;
     int i = 0;
 
 	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown (KeyCode.Space)) {
+	void Update () 
+    {
+        if (Input.GetKeyDown (KeyCode.Space)) 
+        {
             if (i < speech.Length)
                 Raelin.Say(speech[i]);
             else
                 DialogueSystem.instance.Close();
 
             i++;
+        }
+        if (Input.GetKey (KeyCode.M)) 
+        {
+            Raelin.Move(Vector3.left * amt)   
         }
 		
 	}
